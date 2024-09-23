57d62d8a3df2773eecea9eac21d9d58b08b13f63
yessenali <y.zhanaidar@vlife.kz>
2024-09-23 16:00:18 +0500

    Update PanModalPresentationController.swift

--- PanModal/Controller/PanModalPresentationController.swift
+++ PanModal/Controller/PanModalPresentationController.swift
@@ -39,7 +39,7 @@ open class PanModalPresentationController: UIPresentationController {
     struct Constants {
         static let indicatorYOffset = CGFloat(8.0)
         static let snapMovementSensitivity = CGFloat(0.7)
-        static let dragIndicatorSize = CGSize(width: 36.0, height: 5.0)
+        static let dragIndicatorSize = CGSize(width: 64, height: 4)
     }
 
     // MARK: - Properties
